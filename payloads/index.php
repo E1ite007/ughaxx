af169084bf5f470cea5c80d905174d18acd9dfad
E1ite007 <65469983+E1ite007@users.noreply.github.com>
2021-04-18 01:29:17 -0500

    Add files via upload

---
+++ payloads/index.php
@@ -0,0 +1,4 @@
+<a style="font-size: 40px;" href="index-hax.php">HAXX</a><br>
+
+It's important to start the exploit by clicking on a link, visting the exploit site directly probably will crash. <br>
+<b> Make sure to have a valid "payload.elf" on your sd card. </b>
